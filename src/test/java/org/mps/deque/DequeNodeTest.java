4316b4bf3e9d22dc565620360b1ef37ca9cc9069
mxke17 <80281934+mxke17@users.noreply.github.com>
2023-03-09 19:37:33 +0100

    Some fixes of test cases, comments and .gitignore

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -1,13 +1,42 @@
 package org.mps.deque;
 
-//import static javax.swing.text.rtf.RTFAttributes.BooleanAttribute.True;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Nested;
 
+/**
+ * @Author Miguel Jurado Vázquez
+ */
 
-class   DequeNodeTest {
+/**
+ * TESTS DESCRIPTION
+ * Before tests, I have created a list of node: Node1 -> Node 2 -> Node 3
+ *
+ * 1. Test case for getters methods
+ *  - getItemMethodReturnsExpectedValue, check if it returns the correct item
+ *  - getPreviousMethodReturnsExpectedValue, check if it returns the previous node correctly
+ *  - getPreviousMethodReturnsNullForFirstNode, check that the previous node of the first one is null
+ *  - getNextMethodReturnsNullForLastNode, check that the next node of the last one is null
+ *  - getNextMethodReturnsExpectedValue, check if it returns the next node correctly
+ *  - getItemMethodReturnsNullForEmptyNode, check that the item of an empty node is null
+ *
+ * 2. Test cases for setter methods
+ *  - setItemMethod, check that the new item it's setter correctly
+ *  - setPreviousMethod, check that the previous node it's setter correctly
+ *  - setNextMethod, check that the next node it's setter correctly
+ *
+ * 3. Test cases for boolean methods
+ *  - isFirstNodeMethodReturnsExpectedValue,
+ *      Check two conditions: a first node (true condition) and the second node (false condition)
+ *  - isLastNodeMethodReturnsExpectedValue,
+ *      Check two conditions: a last node (true condition) and the first one (false condition)
+ *  - isNotATerminalNodeReturnsExpectedValue,
+ *      Check three conditions: a first node (not fulfil both conditions of terminal node),
+ *      the last node (not fulfil both conditions of terminal node) and the second node (fulfil both conditions)
+ */
+
+class DequeNodeTest {
     DequeNode<Integer> node1 = new DequeNode<Integer>(1, null, null);
     DequeNode<Integer> node2 = new DequeNode<Integer>(2, null, null);
     DequeNode<Integer> node3 = new DequeNode<Integer>(3, null, null);
@@ -104,16 +133,20 @@ class   DequeNodeTest {
 
         @Test
         void isFirstNodeMethodReturnsExpectedValue(){
+            assertTrue(node1.isFirstNode());
             assertFalse(node2.isFirstNode());
         }
 
         @Test
         void isLastNodeMethodReturnsExpectedValue(){
             assertTrue(node3.isLastNode());
+            assertFalse(node1.isLastNode());
         }
 
         @Test
         void isNotATerminalNodeReturnsExpectedValue(){
+            assertTrue(node2.isNotATerminalNode());
+            assertFalse(node1.isNotATerminalNode());
             assertFalse(node3.isNotATerminalNode());
         }
     }
