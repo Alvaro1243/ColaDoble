d0aa27900ef143e19413b301786e75c19a90ec10
mxke17 <80281934+mxke17@users.noreply.github.com>
2023-03-17 21:01:37 +0100

    Refactor DequeNode test

--- src/test/java/org/mps/deque/DequeNodeTest.java
+++ src/test/java/org/mps/deque/DequeNodeTest.java
@@ -1,6 +1,7 @@
 package org.mps.deque;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Nested;
@@ -11,143 +12,204 @@ import org.junit.jupiter.api.Nested;
 
 /**
  * TESTS DESCRIPTION
- * Before tests, I have created a list of node: Node1 -> Node 2 -> Node 3
+ * Given a dequeNode with an item, a previous DequeNode and next DequeNode
+ *  - when calling getItem method, then return the item => testGetItemReturnsExpectedItem()
+ *  - when calling getPrevious method, then return the previous node => testGetPreviousReturnsExpectedNode()
+ *  - when calling getNext method, then return the next node => testGetNextReturnsExpectedNode()
+ *  - when calling setItem method, then set the item => testSetItem()
+ *  - when calling setPrevious method, then set the previous node => testSetPrevious()
+ *  - when calling setNext method, then set the next node => testSetNext()
+ *  - when calling isFirstNode method, then return true => testIsFirstNodeTrue()
+ *  - when calling isFirstNode method, then return false => testIsFirstNodeFalse()
+ *  - when calling isLastNode, then return false => testIsLastNodeFalse()
+ *  - when calling isNotATerminalNode method, then return true => testIsNotATerminalNodeTrue()
  *
- * 1. Test case for getters methods
- *  - getItemMethodReturnsExpectedValue, check if it returns the correct item
- *  - getPreviousMethodReturnsExpectedValue, check if it returns the previous node correctly
- *  - getPreviousMethodReturnsNullForFirstNode, check that the previous node of the first one is null
- *  - getNextMethodReturnsNullForLastNode, check that the next node of the last one is null
- *  - getNextMethodReturnsExpectedValue, check if it returns the next node correctly
- *  - getItemMethodReturnsNullForEmptyNode, check that the item of an empty node is null
+ * Given a dequeNode with just an item (no previous DequeNode and no next DequeNode)
+ *  - when calling getPrevious method, then return null => testGetPreviousReturnsNull()
+ *  - when calling getNext method, then return null => testGetNextReturnsNull()
+ *  - when calling isFirstNode method, then return true => testIsFirstNodeTrue()
+ *  - when calling isLastNode method, then return true => testIsLastNodeTrue()
+ *  - when calling isNotATerminalNode method, then return false => testIsNotATerminalNodeFalse()
  *
- * 2. Test cases for setter methods
- *  - setItemMethod, check that the new item it's setter correctly
- *  - setPreviousMethod, check that the previous node it's setter correctly
- *  - setNextMethod, check that the next node it's setter correctly
+ * Given a dequeNode with an item and previous DequeNode (no next DequeNode)
+ *  - when calling isNotATerminalNode method, then return false => testIsNotATerminalNodeFalse
  *
- * 3. Test cases for boolean methods
- *  - isFirstNodeMethodReturnsExpectedValue,
- *      Check two conditions: a first node (true condition) and the second node (false condition)
- *  - isLastNodeMethodReturnsExpectedValue,
- *      Check two conditions: a last node (true condition) and the first one (false condition)
- *  - isNotATerminalNodeReturnsExpectedValue,
- *      Check three conditions: a first node (not fulfil both conditions of terminal node),
- *      the last node (not fulfil both conditions of terminal node) and the second node (fulfil both conditions)
+ * Given a dequeNode with an item and next DequeNode (no previous DequeNode)
+ *  - when calling isNotATerminalNode method, then return false =>
  */
 
-class DequeNodeTest {
-    DequeNode<Integer> node1 = new DequeNode<Integer>(1, null, null);
-    DequeNode<Integer> node2 = new DequeNode<Integer>(2, null, null);
-    DequeNode<Integer> node3 = new DequeNode<Integer>(3, null, null);
+@DisplayName("A DequeNode")
+class DequeNodeTest<T>{
 
-    @BeforeEach
-    void setUp(){
-        node1.setNext(node2);
-        node2.setPrevious(node1);
-        node2.setNext(node3);
-        node3.setPrevious(node2);
-    }
+    @Nested
+    @DisplayName("with item, previous, and next")
+    class DequeNodeWithItemPreviousNext{
+        T item;
+        DequeNode<T> previous;
+        DequeNode<T> next;
+        DequeNode<T> dequeNode;
 
-    @AfterEach
-    void tearDown(){
-        node1.setPrevious(null);
-        node1.setNext(null);
-        node2.setPrevious(null);
-        node2.setNext(null);
-        node3.setPrevious(null);
-        node3.setNext(null);
+        @BeforeEach
+        void setUp() {
+            item = (T) "Item";
+            previous = new DequeNode<T>((T) "Previous", null, null);
+            next = new DequeNode<T>((T) "Next", null, null);
+            dequeNode = new DequeNode<T>(item, previous, next);
+        }
+
+        @Test
+        @DisplayName("when calling getItem method, then return the item")
+        void testGetItemReturnsExpectedItem() {
+            assertEquals(item, dequeNode.getItem());
+        }
+
+        @Test
+        @DisplayName("when calling getPrevious method, then return the previous node")
+        void testGetPreviousReturnsExpectedNode() {
+            assertEquals(previous, dequeNode.getPrevious());
+        }
+
+        @Test
+        @DisplayName("when calling getNext method, then return the next node")
+        void testGetNextReturnsExpectedNode() {
+            assertEquals(next, dequeNode.getNext());
+        }
+
+        @Test
+        @DisplayName("when calling setItem method, then set the item")
+        void testSetItem() {
+            T newItem = (T) "New Item";
+            dequeNode.setItem(newItem);
+            assertEquals(newItem, dequeNode.getItem());
+        }
+
+        @Test
+        @DisplayName("when calling setPrevious method, then set the previous node")
+        void testSetPrevious() {
+            DequeNode<T> newPrevious = new DequeNode<T>((T) "New Previous", null, null);
+            dequeNode.setPrevious(newPrevious);
+            assertEquals(newPrevious, dequeNode.getPrevious());
+        }
+
+        @Test
+        @DisplayName("when calling setNext method, then set the next node")
+        void testSetNext() {
+            DequeNode<T> newNext = new DequeNode<T>((T) "New Next", null, null);
+            dequeNode.setNext(newNext);
+            assertEquals(newNext, dequeNode.getNext());
+        }
+
+        @Test
+        @DisplayName("when calling isFirstNode method, then return true")
+        void testIsFirstNodeTrue() {
+            previous = null;
+            dequeNode = new DequeNode<T>(item, previous, next);
+            assertTrue(dequeNode.isFirstNode());
+        }
+
+        @Test
+        @DisplayName("when calling isFirstNode method, then return false")
+        void testIsFirstNodeFalse() {
+            assertFalse(dequeNode.isFirstNode());
+        }
+
+        @Test
+        @DisplayName("when calling isLastNode, then return false")
+        void testIsLastNodeFalse() {
+            assertFalse(dequeNode.isLastNode());
+        }
+
+        @Test
+        @DisplayName("when calling isNotATerminalNode method, then return true")
+        void testIsNotATerminalNodeTrue(){
+            assertTrue(dequeNode.isNotATerminalNode());
+        }
     }
 
     @Nested
-    @DisplayName("Test cases for getters methods")
-    class TestsCasesForGettersMethods{
+    @DisplayName("with only a item (no previous DequeNode and no next DequeNode)")
+    class DequeNodeWithOnlyAItem<T>{
+        T item;
+        DequeNode<T> dequeNode;
+
+        @BeforeEach
+        void setUp() {
+            item = (T) "Item";
+            dequeNode = new DequeNode<T>(item, null, null);
+        }
+
 
         @Test
-        void getItemMethodReturnsExpectedValue(){
-            assertEquals(1, node1.getItem());
+        @DisplayName("when calling getPrevious method, then return null")
+        void testGetPreviousReturnsNull() {
+            assertNull(dequeNode.getPrevious());
         }
 
         @Test
-        void getPreviousMethodReturnsExpectedValue(){
-            assertSame(node1, node2.getPrevious());
+        @DisplayName("when calling getNext method, then return null")
+        void testGetNextReturnsNull() {
+            assertNull(dequeNode.getNext());
         }
 
         @Test
-        void getPreviousMethodReturnsNullForFirstNode(){
-            assertNull(node1.getPrevious());
+        @DisplayName("when calling isFirstNode method, then return true")
+        void testIsFirstNodeTrue() {
+            assertTrue(dequeNode.isFirstNode());
         }
 
         @Test
-        void getNextMethodReturnsNullForLastNode(){
-            assertNull(node3.getNext());
+        @DisplayName("when calling isLastNode method, then return true")
+        void testIsLastNodeTrue() {
+            assertTrue(dequeNode.isLastNode());
         }
 
         @Test
-        void getNextMethodReturnsExpectedValue(){
-            assertSame(node3, node2.getNext());
+        @DisplayName("when calling isNotATerminalNode method, then return false")
+        void testIsNotATerminalNodeFalse(){
+            assertFalse(dequeNode.isNotATerminalNode());
+        }
+
+    }
+
+    @Nested
+    @DisplayName("with item, previous DequeNode and no next DequeNode")
+    class DequeNodeClassWithItemAndPrevious{
+        T item;
+        DequeNode<T> dequeNode;
+        DequeNode<T> previous;
+
+        @BeforeEach
+        void setUp() {
+            item = (T) "Item";
+            previous = new DequeNode<T>((T) "Previous", null, null);
+            dequeNode = new DequeNode<T>(item, previous, null);
         }
 
         @Test
-        void getItemMethodReturnsNullForEmptyNode() {
-            DequeNode<Integer> emptyNode = new DequeNode<>(null, null, null);
-            assertNull(emptyNode.getItem());
+        @DisplayName("when calling isNotATerminalNode method, then return false")
+        void testIsNotATerminalNodeFalse(){
+            assertFalse(dequeNode.isNotATerminalNode());
         }
     }
 
-
     @Nested
-    @DisplayName("Tests cases for setters methods")
-    class TestsCasesForSettersMethods{
+    @DisplayName("with item, next DequeNode and no previous DequeNode")
+    class DequeNodeClassWithItemAndNext {
+        T item;
+        DequeNode<T> dequeNode;
+        DequeNode<T> next;
 
-        @Test
-        void setItemMethod(){
-            node1.setItem(5);
-
-            assertEquals(5, node1.getItem());
+        @BeforeEach
+        void setUp() {
+            item = (T) "Item";
+            next = new DequeNode<T>((T) "Next", null, null);
+            dequeNode = new DequeNode<T>(item, null, next);
         }
 
         @Test
-        void setPreviousMethod(){
-            DequeNode<Integer> node0 = new DequeNode<Integer>(0, null, null);
-
-            node1.setPrevious(node0);
-
-            assertSame(node0, node1.getPrevious());
-        }
-
-        @Test
-        void setNextMethod(){
-            DequeNode<Integer> node4 = new DequeNode<Integer>(4, null, null);
-
-            node3.setNext(node4);
-
-            assertSame(node4, node3.getNext());
-        }
-    }
-
-
-    @Nested
-    @DisplayName("Test cases for boolean methods")
-    class TestCasesForBooleanMethods{
-
-        @Test
-        void isFirstNodeMethodReturnsExpectedValue(){
-            assertTrue(node1.isFirstNode());
-            assertFalse(node2.isFirstNode());
-        }
-
-        @Test
-        void isLastNodeMethodReturnsExpectedValue(){
-            assertTrue(node3.isLastNode());
-            assertFalse(node1.isLastNode());
-        }
-
-        @Test
-        void isNotATerminalNodeReturnsExpectedValue(){
-            assertTrue(node2.isNotATerminalNode());
-            assertFalse(node1.isNotATerminalNode());
-            assertFalse(node3.isNotATerminalNode());
+        @DisplayName("when calling isNotATerminalNode method, then return false")
+        void testIsNotATerminalNodeFalse(){
+            assertFalse(dequeNode.isNotATerminalNode());
         }
     }
 }
